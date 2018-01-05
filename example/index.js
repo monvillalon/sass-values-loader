f7f4d1bb0d7937f97fc15e96ae8feb959635b377
Jerry Jäppinen <eiskis@gmail.com>
2018-01-05 14:37:07 +0100

    Cleaned up sample index file

--- example/index.js
+++ example/index.js
@@ -1,8 +1,10 @@
-import styles from './style.scss';
-import vars from "!!vars!./style.scss";
+import styles from './style.scss'
 
-console.log("STYLE==============");
-console.log(styles);
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import vars from '!!vars!./style.scss'
 
-console.log("VARS==============");
-console.log(vars);
+console.log('STYLE==============')
+console.log(styles)
+
+console.log('VARS==============')
+console.log(vars)
